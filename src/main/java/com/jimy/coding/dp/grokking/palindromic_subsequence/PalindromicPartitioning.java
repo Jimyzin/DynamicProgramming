b52d7c7acc62d0bea4c01cd9f87a9051b120550b
sujoykbhattacharya@gmail.com <sujoykbhattacharya@gmail.com>
2020-06-12 21:39:32 +0800

    Added alternative approach for geeksforgeeks-PalindromicPartitioning

--- src/main/java/com/jimy/coding/dp/grokking/palindromic_subsequence/PalindromicPartitioning.java
+++ src/main/java/com/jimy/coding/dp/grokking/palindromic_subsequence/PalindromicPartitioning.java
@@ -38,24 +38,82 @@ public class PalindromicPartitioning {
 
     }
 
+    /**
+     * Approach 2: This is a dynamic programming approach that calculates the minimum number of palindromic substrings in a given string.
+     * This, the number of partitions (or cuts) = one less tha the above value.
+     *
+     */
+    static int countPartitionsDP2(String s) {
+        int len = s.length();
+        int[][] dp = new int[len][len]; // dp[startIndex][endIndex] represents the count of minimum of palindromic partitions between startIndex and endIndex.
+
+        //all elements are palindrome of length at their corresponding indices, resulting in all 1's along the diagonal.
+        for (int index = 0; index < len; index++) {
+            dp[index][index] = 1;
+        }
+
+        // fill the rest of the elements of the top-half array diagonally
+        for (int diff = 1; diff < len; diff++) {
+            for (int startIndex = 0, endIndex = startIndex + diff; endIndex < len; startIndex++, endIndex++) {
+
+                // if a match is found it may be a palindrome (eg: dbd) or may not (eg: ddpd)
+                if (s.charAt(startIndex) == s.charAt(endIndex)) {
+
+                    // it is a palindrome if startIndex is adjacent to endIndex, i.e. a string of length 2 (eg: dd)
+                    // else if the rest of the string excluding the startIndex and endIndex is palindrome,
+                    //                          i.e s(startIndex + 1, endIndex - 1) = 1 palindromic string (eg: m -->ada<-- m)
+                    if (diff == 1 || dp[startIndex + 1][endIndex - 1] == 1) {
+                        dp[startIndex][endIndex] = 1;
+
+                    } else {
+
+                        // if the rest of the string excluding the startIndex and endIndex is not a palindrome,
+                        // i.e s(startIndex + 1, endIndex - 1) > 1, the minimum number of palindromic strings = MINIMUM_VALUE {
+                             // 1(element at startIndex) + min. number of palindromes in s(startIndex + 1, endIndex),
+                             // 1(element at endIndex) + min. number of palindromes in s(startIndex, endIndex - 1),
+                             // 2(element at starIndex + element at endIndex) + min. number of palindromes in s(startIndex + 1, endIndex - 1) }
+                        dp[startIndex][endIndex] = Math.min(Math.min( (1 + dp[startIndex][endIndex - 1]), (1 + dp[startIndex + 1][endIndex]) ),
+                                2 + dp[startIndex + 1][endIndex - 1]);
+                    }
+
+                } else {
+
+                    // if no match is found, the minimum number of palindromic strings = MINIMUM_VALUE {
+                       // 1(element at startIndex) + min. number of palindromes in s(startIndex + 1, endIndex),
+                       // 1(element at endIndex) + min. number of palindromes in s(startIndex, endIndex - 1) }
+                    dp[startIndex][endIndex] = 1 + Math.min(dp[startIndex][endIndex - 1], dp[startIndex + 1][endIndex]);
+                }
+            }
+        }
+        DPUtils.print2DArray(dp, len, len);
+
+        //Minimum number of partitions = Minimum number of palindromes - 1
+        return dp[0][len - 1] - 1;
+    }
+
     public static void main(String[] args) {
         System.out.println("--------------abdbca----------------");
         System.out.println("Result (countPartitionsDP):" + countPartitionsDP("abdbca"));
+        System.out.println("Result (countPartitionsDP2):" + countPartitionsDP2("abdbca"));
         System.out.println();
 
         System.out.println("--------------cddpd----------------");
         System.out.println("Result (countPartitionsDP):" + countPartitionsDP("cddpd"));
+        System.out.println("Result (countPartitionsDP2):" + countPartitionsDP2("cddpd"));
         System.out.println();
 
         System.out.println("--------------pqr----------------");
         System.out.println("Result (countPartitionsDP):" + countPartitionsDP("pqr"));
+        System.out.println("Result (countPartitionsDP2):" + countPartitionsDP2("pqr"));
         System.out.println();
 
         System.out.println("--------------pp----------------");
         System.out.println("Result (countPartitionsDP):" + countPartitionsDP("pp"));
+        System.out.println("Result (countPartitionsDP2):" + countPartitionsDP2("pp"));
         System.out.println();
 
         System.out.println("--------------madam----------------");
         System.out.println("Result (countPartitionsDP):" + countPartitionsDP("madam"));
+        System.out.println("Result (countPartitionsDP2):" + countPartitionsDP2("madam"));
     }
 }
